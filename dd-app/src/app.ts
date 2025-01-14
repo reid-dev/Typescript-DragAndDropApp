7c9723a494e8cfb9961cca2d6162050bde37ffc9
Jamie Reid <jare@co.ulster.ny.us>
2025-01-14 11:36:57 -0500

    Added experimental decorators for autobinding

--- dd-app/src/app.ts
+++ dd-app/src/app.ts
@@ -1,8 +1,25 @@
+// Autobind Decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFxn = originalMethod.bind(this);
+            return boundFxn;
+        }
+    }
+    return adjDescriptor;
+}
+
+// Prohejc Input Class
 class ProjectInput {
 
     templateELement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateELement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -10,7 +27,29 @@ class ProjectInput {
 
         const importedNode = document.importNode(this.templateELement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        // Access to different inputs on form
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+
+        this.configure();
         this.attach();
+    }
+
+    // Handes Form Submissions
+    @autobind
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+
+    }
+
+    // Set up event listeners
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler);
     }
 
     private attach() {
