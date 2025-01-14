e54d8eeca0b042542abe7fab150cef36c5adb94e
Jamie Reid <jare@co.ulster.ny.us>
2025-01-14 11:22:06 -0500

    First form created and rendered

--- dd-app/src/app.ts
+++ dd-app/src/app.ts
@@ -1,1 +1,21 @@
-// Code goes here!
+class ProjectInput {
+
+    templateELement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+
+    constructor() {
+        this.templateELement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateELement.content, true);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.attach();
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+const projInput = new ProjectInput();
